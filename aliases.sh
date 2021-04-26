3cc2b4df6b90f3942259ee5373d0edd65176c1cf
zzacal <zac.zacal@alaskaair.com>
2021-04-26 13:32:38 -0700

    Added gex
    gex is a tool that quickly edits .git/info/exclude

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,18 @@ newbs() {
     chmod 775 $1 && \
     echo '#! /bin/bash\necho "Hello World!"' > $1 && \
 }
+
+gex() {
+    if [ -z $1 ]
+    then
+        echo 'current exclude:'
+        cat $(git rev-parse --show-toplevel)/.git/info/exclude
+        echo '\nto add more, specify a pattern. e.g.:'
+        echo '% gex tools/**.*\n'
+        return 0
+    else
+        echo 'adding' $1 'to' $(git rev-parse --show-toplevel)'/.git/info/exclude'
+        echo $1 >> $(git rev-parse --show-toplevel)/.git/info/exclude
+        return 0
+    fi
+}
