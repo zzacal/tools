ca878b0addff9a846d408e66f0119f36771fac7d
zzacal <jizacal@gmail.com>
2022-01-11 22:25:50 -1000

    Scripted git ignore workflow

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 alias ibrew='arch -x86_64 /usr/local/bin/brew'
 alias mbrew='arch -arm64e /opt/homebrew/bin/brew'
 
-alias cat="ccat"
+alias cat='ccat'
 
 newbs() {
     touch $1 && \
@@ -20,6 +20,28 @@ gex() {
     else
         echo 'adding' $1 'to' $(git rev-parse --show-toplevel)'/.git/info/exclude'
         echo $1 >> $(git rev-parse --show-toplevel)/.git/info/exclude
+        return 0
+    fi
+}
+
+gig() {
+    FILE=$(git rev-parse --show-toplevel)/.gitignore
+    echo $FILE
+    # create .gitignore if needed
+    if ! test -f "$FILE"; then
+        echo "Info: Creating $FILE"
+        touch $FILE
+    fi
+
+    if [ -z $1 ]; then
+        echo 'current exclude:'
+        cat $FILE
+        echo '\nto add more, specify a pattern. e.g.:'
+        echo '% gig tools/**.*\n'
+        return 1
+    else
+        echo "adding $1 to $FILE"
+        echo $1 >> $FILE
         return 0
     fi
 }
